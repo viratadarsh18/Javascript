a9cc8698435c3d1287ecf4735a69357b35f44862
Adarsh Rai <81464267+viratadarsh18@users.noreply.github.com>
2023-10-11 12:45:35 +0000

    New

---
+++ Basic/01_Variables.js
@@ -0,0 +1,14 @@
+const accountId = 123
+let email ="adarshrai911866@gmail.com"
+var password = "1234"
+city = "jaipur"
+let prit;
+
+//accountId = 324
+email = "adarshrai123@gmail.com"
+password = "2122"
+city = "Varanasi"
+console.log(accountId)
+console.table([accountId, email, password, city, prit])
+
+// Dont use var because it has scope issue. 
